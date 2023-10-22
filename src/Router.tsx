0afcfbed875ab5f2451f51f634732dcc876dad8d
fujitargz <61151276+fujitargz@users.noreply.github.com>
2023-10-22 21:15:12 +0900

    [chore] fix router config

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Root } from './pages/Root'
 import { Login } from './pages/Login'
+import { RentFrom } from './pages/RentFrom'
+import { RentTo } from './pages/RentTo'
 
-export const Router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-])
+export const Router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/rentfrom',
+      element: <RentFrom />,
+    },
+    {
+      path: '/rentto',
+      element: <RentTo />,
+    },
+  ],
+  { basename: '/ict-solution' },
+)
