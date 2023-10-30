981bbfc9c377b3300790fda77229eea73034b6ec
fujitargz <61151276+fujitargz@users.noreply.github.com>
2023-10-30 11:05:07 +0900

    [fix] msw error on gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { worker } from './mocks/browser.ts'
+import packageJson from '../package.json'
 
-worker
-  .start({
-    onUnhandledRequest: 'bypass',
-    serviceWorker: { options: { scope: '/ict-solution' } },
-  })
-  .then(() => {
-    ReactDOM.createRoot(document.getElementById('root')!).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
-  })
+const mswConfig =
+  process.env.NODE_ENV === 'development'
+    ? {
+        onUnhandledRequest: 'bypass',
+        serviceWorker: {
+          options: { scope: '/ict-solution' },
+        },
+      }
+    : {
+        onUnhandledRequest: 'bypass',
+        serviceWorker: {
+          options: { scope: '/ict-solution' },
+          url: `${packageJson.homepage}/mockServiceWorker.js`,
+        },
+      }
+
+worker.start(mswConfig).then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
