d223185550a99141286af9c8fa674e7a9a855f6f
fujitargz <61151276+fujitargz@users.noreply.github.com>
2023-10-30 12:09:35 +0900

    [fix] leaflet marker icon

--- src/components/Frame/Map/Map.tsx
+++ src/components/Frame/Map/Map.tsx
@@ -1,4 +1,14 @@
+import L from 'leaflet'
 import { Marker, MapContainer, TileLayer } from 'react-leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+})
+
+L.Marker.prototype.options.icon = DefaultIcon
 
 export const Map = () => (
   <MapContainer
