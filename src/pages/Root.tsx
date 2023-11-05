f0d21de2f55bcebf2afd10f1720f707c84ab7750
fujitargz <61151276+fujitargz@users.noreply.github.com>
2023-11-05 11:48:32 +0900

    [feat] status

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,8 +2,11 @@ import { Navigate, useNavigate } from 'react-router-dom'
 import { Button, Space, Stack, Text } from '@mantine/core'
 import { useSession, checkSessionManually } from '../hooks/useSession'
 import { useSetSession, setSessionManually } from '../hooks/useSetSession'
+import { useState } from 'react'
+import { endpoint, Battery, Rental } from '../mocks/handlers'
 
 export const Root = () => {
+  const [status, setStatus] = useState('')
   const [session] = useSession()
   const setSession = useSetSession()
   const navigate = useNavigate()
@@ -18,8 +21,37 @@ export const Root = () => {
     }
   }
 
+  const getStatusText = (status: string) => {
+    switch (status) {
+      case 'available':
+        return '募集中'
+      case 'reserved':
+        return 'レンタル待ち'
+      default:
+        return '-'
+    }
+  }
+  fetch(endpoint('battery', 'owner', session.currentUser.id))
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error()
+      }
+      return res.json()
+    })
+    .then((body) => (body.battery as Battery).id)
+    .then((batteryId) => fetch(endpoint('rental', 'battery', batteryId)))
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error()
+      }
+      return res.json()
+    })
+    .then((body) => setStatus((body.rental as Rental).status))
+    .catch(() => {})
+
   return (
     <Stack>
+      <Text>ステータス：{getStatusText(status)}</Text>
       <Button variant="filled" onClick={() => navigate('/rentfrom')}>
         モバイルバッテリーを借りる
       </Button>
